0a17cd5f0763291cddaef810ececc72e9917ee78
fkfaran273 <faran@appanalytics.in>
2025-09-13 16:28:42 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Cards from "./components/Cards"
+import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // put items in array and use map function to render cards
+  const items = [
+    { id: 1, cardname: "anurag", image_src: "https://images.pexels.com/photos/1181359/pexels-photo-1181359.jpeg" },
+    { id: 2, cardname: "anurag", image_src: "https://images.pexels.com/photos/1181359/pexels-photo-1181359.jpeg" },
+    { id: 3, cardname: "anurag", image_src: "https://images.pexels.com/photos/1181359/pexels-photo-1181359.jpeg" },
+  ];
+
+  return ( 
+    <>
+    <Navbar/>
+  <div className="d-flex gap-4 justify-content-center pt-5">
+    {items.map(item => (<Cards key={item.id} cardname={item.cardname} image_src={item.image_src} />))}
+   </div>
+   </>)
 }
 
 export default App;
+
---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+function Cards(props){
+    return (
+        <div className="card" style={{ width: "18rem"}}>
+            <img className="card-img-top" src={props.image_src} alt="Card cap" />
+            <div className="card-body">
+                <h5 className="card-title">Python programming</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="/" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    )
+}
+export default Cards;
