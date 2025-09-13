5370f82b6da33af43bb7a11310e861ebe160f78e
fkfaran273 <faran@appanalytics.in>
2025-09-13 17:07:18 +0530

    initial

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,7 +3,7 @@ function Cards(props){
         <div className="card" style={{ width: "18rem"}}>
             <img className="card-img-top" src={props.image_src} alt="Card cap" />
             <div className="card-body">
-                <h5 className="card-title">{props.title}</h5>
+                <h5 className="card-title">{props.cardname}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="/" className="btn btn-primary">Go somewhere</a>
             </div>
